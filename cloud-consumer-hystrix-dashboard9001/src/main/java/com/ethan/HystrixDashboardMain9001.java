b279ebeef7ffa70817a280f79ad0182d2d081e4d
ethan <wangyx029@outlook.com>
2020-09-20 17:37:57 +0800

    hystrix服务熔断和Dashboard展示

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/ethan/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.ethan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName : HystrixDashboardMain9001
+ * @Author : ethan
+ * @Date: 2020-09-20 17:26
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
