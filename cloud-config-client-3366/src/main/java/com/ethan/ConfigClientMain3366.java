d12d278a06ec2b9398f37e35444e09173c8798bd
ethan <wangyx029@outlook.com>
2020-09-27 21:27:32 +0800

    config动态刷新的全局通知和定点通知

---
+++ cloud-config-client-3366/src/main/java/com/ethan/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.ethan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName : ConfigClientMain3366
+ * @Author : ethan
+ * @Date: 2020-09-27 20:54
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
